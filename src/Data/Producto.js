3d5654c27760f0bb9c5e09a1936951870cea7f91
alexeduardo124 <alexeduardo2@hotmail.com>
2024-08-19 21:45:41 -0500

    proyecto-final

---
+++ src/Data/Producto.js
@@ -0,0 +1,30 @@
+export let Producto = [
+  {
+    image: "/src/assets/yellow.png",
+    nombre: "Air Max 97",
+    precio: "$20.99",
+    talla:"40 41 42 43 "
+  },
+
+  {
+    image: "/src/assets/Red.png",
+    nombre: "React Presto",
+    precio: "$25.99",
+    talla:"35 36 37 39 "
+
+  },
+
+  {
+    image: "/src/assets/Green.png",
+    nombre: "Air flex ",
+    precio: "$28.99",
+    talla:"38 41 42 44 "
+  },
+
+  {
+    image: "/src/assets/toppng.png",
+    nombre: "comand",
+    precio: "$30.99",
+    talla:"39 40 42 44 "
+  },
+];
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const main = () => {
+  return (
+    <div>main</div>
+  )
+}
+
